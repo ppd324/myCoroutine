befbf7d689ff10cdeeb4297a3a8f97d7c7ff73a7
裴沛东 <1326845559@qq.com>
2022-06-07 22:29:10 +0800

    增加线程池以及Task类

--- main.cpp
+++ main.cpp
@@ -3,41 +3,45 @@
 #include <iostream>
 #include <unistd.h>
 #include <cstring>
+#include "TcpServer.h"
 
 int main() {
-    Mycoroutine *mycoroutine = Mycoroutine::mycoroutine();
-    mycoroutine->co_create([&]() {
-        Listener listener;
-        listener.ListenTCP(8888);
-        while(true) {
-            int client_fd = listener.Accept();
-            if(client_fd < 0) {
-                continue;
-            }
-            printf("client connected\n");
-            Connection conn(client_fd);
-            mycoroutine->co_create([&]() {
-                char buf[128] = {0};
-                while(true) {
-                    memset(buf,0,128);
-                    int n = conn.Read(buf,128);
-                    if(n <= 0) {
-                        printf("close fd\n");
-                        conn.Close();
-                        break;
-                    }
-                    printf("recv: %s",buf);
-                    n = conn.Write(buf,n);
-                    if(n <= 0) {
-                        printf("close fd\n");
-                        conn.Close();
-                        break;
-                    }
-                }
-            });
+//    Mycoroutine *mycoroutine = Mycoroutine::mycoroutine();
+//    mycoroutine->co_create([&]() {
+//        Listener listener;
+//        listener.ListenTCP(8888);
+//        while(true) {
+//            int client_fd = listener.Accept();
+//            if(client_fd < 0) {
+//                continue;
+//            }
+//            printf("client connected\n");
+//            Connection conn(client_fd);
+//            mycoroutine->co_create([&]() {
+//                char buf[128] = {0};
+//                while(true) {
+//                    memset(buf,0,128);
+//                    int n = conn.Read(buf,128);
+//                    if(n <= 0) {
+//                        printf("close fd\n");
+//                        conn.Close();
+//                        break;
+//                    }
+//                    printf("recv: %s",buf);
+//                    n = conn.Write(buf,n);
+//                    if(n <= 0) {
+//                        printf("close fd\n");
+//                        conn.Close();
+//                        break;
+//                    }
+//                }
+//            });
+//
+//        }
+//    });
+//    mycoroutine->co_dispatch();
+    TcpServer tcpServer(8000,5);
 
-        }
-    });
-    mycoroutine->co_dispatch();
+
     return 0;
 }
